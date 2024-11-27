36df804e5124ec81ae223c87e4f27b36d718fb3e
gitaufar <zhafiraufar123@gmail.com>
2024-11-27 11:06:46 +0700

    mengubah menjadi grid

--- src/sections/ProfileSection.jsx
+++ src/sections/ProfileSection.jsx
@@ -1,28 +1,35 @@
 import React from "react";
-import { Container, Typography, Button, Icon } from "@mui/material";
-import Grid from "@mui/material/Grid2";
+import { Grid, Typography, Button } from "@mui/material";
 import { ReactComponent as Logo } from '../rescue_logo.svg';
 
 function ProfileSection() {
   return (
     <div className="profile_container" id="profile_section">
       <div className="nav_container">
-      <a href="#profile_section">Home</a>
+        <a href="#profile_section">Home</a>
         <a href="#about_section">About</a>
         <a href="#mockup_section">Mockup</a>
         <a href="#prototype_section">Prototype</a>
       </div>
-      <div className="profile_content_container">
+
+      <Grid container className="profile_content_container" alignItems="center" justifyContent="center" spacing={0} style={{ height: '100%' }}>
+        <Grid item xs={12} md={4.5} container justifyContent="center">
           <div className="logo_container">
-            <Logo className="logo"></Logo>
+            <Logo className="logo" />
           </div>
+        </Grid>
+
+        <Grid item xs={12} md={7.5} container justifyContent="center" alignItems="center">
           <div className="profile_text_container">
-            <h1>RESCUE</h1>
-            <h2>Real-time Evaluation for Safety, Care, and Utilization of
-            <br/>Emergency services</h2>
-            <p>an apps for monitoring,emergency, and consultation</p>
+            <Typography variant="h1">RESCUE</Typography>
+            <Typography variant="h2">
+              Real-time Evaluation for Safety, Care, and Utilization of
+              <br /> Emergency services
+            </Typography>
+            <Typography variant="body1">An app for monitoring, emergency, and consultation</Typography>
           </div>
-      </div>
+        </Grid>
+      </Grid>
     </div>
   );
 }
