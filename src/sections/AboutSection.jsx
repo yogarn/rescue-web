03742d4f4f46b3f1a204ad1a2195a429c9523f5e
Yoga Raditya <radityanalaa@gmail.com>
2024-12-01 12:55:53 +0700

    `feat:` Add about us section

--- src/sections/AboutSection.jsx
+++ src/sections/AboutSection.jsx
@@ -1,14 +1,29 @@
-import '../about.css'
+import "../about.css";
 import React from "react";
-import { Container, Typography, Button } from "@mui/material";
-import Grid from "@mui/material/Grid2";
+import { Typography } from "@mui/material";
 
 function AboutSection() {
-    return (
-      <div className="about_container" id='about_section'>
-
+  return (
+    <div className="about_container" id="about_section">
+      <div className="about_content">
+        <Typography variant="h3" className="about_title">
+          Tentang Kami
+        </Typography>
+        <Typography className="about_text">
+          Sistem pemantauan kesehatan efektif sangat penting bagi pasien rawat
+          jalan, terutama dalam mencegah komplikasi dan mendukung pemulihan.
+          Studi menunjukkan 50% pasien kronis sering lupa minum obat atau
+          melakukan pemeriksaan tanpa pemantauan ketat, berisiko memicu
+          komplikasi serius, sementara 30% pasien rawat jalan mengalami cedera
+          akibat jatuh. Inovasi IoT memungkinkan pemantauan real-time untuk
+          detak jantung, tekanan darah, dan saturasi oksigen, yang mempercepat
+          respons saat kondisi darurat dan memudahkan dokter mengakses data
+          untuk evaluasi akurat. Teknologi ini meningkatkan keselamatan pasien,
+          mendukung SDG 3, dan mengurangi biaya perawatan.
+        </Typography>
       </div>
-    );
-  }
-  
-  export default AboutSection;
+    </div>
+  );
+}
+
+export default AboutSection;
