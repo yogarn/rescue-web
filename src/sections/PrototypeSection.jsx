d9f1b36042e4700f93081ec429887c97d03a47a2
Yoga Raditya <radityanalaa@gmail.com>
2024-12-02 21:32:11 +0700

    `feat:` Add prototype youtube iframe

--- src/sections/PrototypeSection.jsx
+++ src/sections/PrototypeSection.jsx
@@ -1,26 +1,47 @@
-import '../prototype.css'
+import "../prototype.css";
 import React from "react";
-import Opening from '../prototype/Opening';
-import { useState } from "react";
 import { Container, Typography, Button } from "@mui/material";
 import Grid from "@mui/material/Grid2";
-import Login from '../prototype/Login';
 
 function PrototypeSection() {
-  const [isOpening, setOpening] = useState(true);
-  const [isLogin, setLogin] = useState(false);
-  const openingClick = () => {
-    setOpening(false)
-    setLogin(true)
-  }
-
-
   return (
-    <div className="prototype_container" id='prototype_section'>
-      <div className="hp">
-      {isOpening ? <Opening nextClick={openingClick}/> : null}
-      {isLogin ? <Login/> : null}
-      </div>
+    <div className="prototype_container" id="prototype_section">
+      <Container>
+        <Grid container spacing={2} alignItems={"center"}>
+          <Grid item xs={12} md={6}>
+            <div className="prototype_video">
+              <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/mmCnQDUSO4I?si=3e9WMHLFMfhGEhxJ"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerpolicy="strict-origin-when-cross-origin"
+                allowfullscreen
+              ></iframe>
+            </div>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <div className="prototype_text">
+              <Typography variant="h4" color="textPrimary">
+                Prototype
+              </Typography>
+              <Typography variant="h6" color="textSecondary">
+                Coba prototype Figma kami sekarang dan lihat bagaimana kami bisa membantu Anda dalam menjaga kesehatan Anda.
+              </Typography>
+              <Button
+                variant="contained"
+                color="primary"
+                size="large"
+                sx={{ marginTop: 2 }}
+              >
+                Get started
+              </Button>
+            </div>
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 }
